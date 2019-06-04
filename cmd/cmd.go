4d0e9d659d9b4e271ab5d0005fdfe41e810e8efb
Vonk <N/A>
2019-06-04 22:13:15 +0200

    Initial commit

---
+++ cmd/cmd.go
@@ -0,0 +1,76 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/spf13/cobra"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var Format string
+
+type VideoLink struct {
+	Title string
+	Href  string
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "youtube-search <search query>",
+	Short: "Searches youtube for given text and outputs it to standard out",
+	Long:  `Searches youtube for given text and outputs it to standard out`,
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		argWords := make([]string, 0)
+		for _, arg := range args {
+			argWords = append(argWords, strings.Fields(arg)...)
+		}
+		mergedArgs := strings.Join(argWords, "+")
+		searchQuery := strings.ReplaceAll(mergedArgs, " ", "+")
+
+		response, err := http.Get("https://www.youtube.com/results?search_query=" + searchQuery)
+		if err != nil {
+			os.Exit(127)
+		}
+		defer response.Body.Close()
+
+		document, err := goquery.NewDocumentFromReader(response.Body)
+		if err != nil {
+			log.Fatal("Error open search result response body. ", err)
+		}
+
+		template, err := template.New("output").Parse(Format)
+		if err != nil {
+			log.Fatal("Could not parse format flag from as go template")
+		}
+
+		document.Find("a").Each(func(_ int, element *goquery.Selection) {
+			title, titleExist := element.Attr("title")
+			href, hrefExist := element.Attr("href")
+
+			if titleExist && hrefExist && strings.HasPrefix(href, "/watch?") {
+				 _ = template.Execute(os.Stdout, VideoLink{
+					title,
+					"https://www.youtube.com" + href,
+				})
+				fmt.Println()
+			}
+		})
+	},
+}
+
+func Execute() {
+	rootCmd.PersistentFlags().StringVarP(
+		&Format,
+		"format",
+		"f",
+		"{{ .Title }} - {{ .Href }}",
+		"Go template to format output.")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module youtube-search
+
+go 1.12
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/google/pprof v0.0.0-20190515194954-54271f7e092f // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20181102032728-5e5cf60278f6 // indirect
+	github.com/spf13/cobra v0.0.4
+	golang.org/x/arch v0.0.0-20190312162104-788fe5ffcd8c // indirect
+	golang.org/x/tools v0.0.0-20190603211825-70bf279967a6 // indirect
+)
